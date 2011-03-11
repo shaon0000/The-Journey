1cc7c3531cbbebe78ca2a36e1c88f9d1371fb9ed
Shaon Bhuiyan <shaon0000@gmail.com>
2011-03-11 14:31:28 -0800

    added registration page

--- journey/creds/views.py
+++ journey/creds/views.py
@@ -29,6 +29,39 @@ def user_login(request):
         c['next'] = request.GET.get('next','/booth/')
         return render_to_response('login.html', c)
 
+import urllib
+
+def error_query_string(error):
+    error = {'error': error}
+    error = urllib.urlencode(error)
+    return '?'+error
+
+def register(request):
+    if request.POST:
+        # registration form was submitted
+        username = request.POST['username']
+        
+        if username.count('@yelp') != 1:
+            return HttpResponseRedirect('/register/' + error_query_string('Yelp accounts only'))
+
+        password = request.POST['password']
+        repeat = request.POST['repeat']
+        if password != repeat:
+            return HttpResponseRedirect('/register/' + error_query_string('passwords must be the same'))
+        try:
+            user = User.objects.create(username, password)
+        except User.AlreadyExistis:
+            return HttpResponseRedirect('/register/' + error_query_string('username already exists, try another one'))
+            
+        return user_login(request)
+    else:
+        # registration form was asked for
+        c = {}
+        c.update(csrf(request))
+        if request.GET:
+            c['error'] = request.GET.get('error', None)
+        return render_to_response('registration.html', c)
+
 @login_required
 def logout_user(request):
   logout(request)
