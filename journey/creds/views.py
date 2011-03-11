81386ad79dd2a6c226c75c146156af7229634339
Shaon Bhuiyan <shaon0000@gmail.com>
2011-03-10 21:18:35 -0800

    added login templates

--- journey/creds/views.py
+++ journey/creds/views.py
@@ -16,16 +16,16 @@ def user_login(request):
                 login(request, user)
                 return HttpResponseRedirect(next)
             else:
-                return render_to_response('login_page.html', {'error': 'Your account has been disabled'})
+                return render_to_response('login.html', {'error': 'Your account has been disabled'})
         else:
-            return render_to_resposne('login_page.html', {'error': 'incorrect password/username combination'})
-    elif request.GET:
-        # lgin form was requested
+            return render_to_resposne('login.html', {'error': 'incorrect password/username combination'})
+    else:
+        # login form was requested
         next = request.GET.get('next','/booth/')
-        return render_to_response('login_page.html',{'next':next})
+        return render_to_response('login.html',{'next':next})
 
 @login_required
-def logoutUser(request):
+def logout_user(request):
   logout(request)
   return HttpResponseRedirect('/login/')
 
--- journey/urls.py
+++ journey/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns(
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.curdir + '/site_media'}),
     (r'^my_votes/$', 'journey.vote.views.get_my_votes'),
     (r'^login/$', 'journey.creds.views.user_login'),
-    (r'^logout/$', 'journey.creds.views.logout'),
+    (r'^logout/$', 'journey.creds.views.logout_user'),
 )
