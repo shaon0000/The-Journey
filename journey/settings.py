f947a99c6bf7993467b57288b319a36697d624fc
Duncan Cook <duncan.cook@theletterd.co.uk>
2011-03-10 17:14:13 -0800

    db and all that

--- journey/settings.py
+++ journey/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'vote'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
