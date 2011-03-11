a2ce7195c3e125a21b4961512dc50171cf8611b7
Evan Krall <krall@yelp.com>
2011-03-11 14:35:53 -0800

    Make the mobile page AJAX some data.

--- journey/site_media/js/journey_mobile.js
+++ journey/site_media/js/journey_mobile.js
@@ -1,153 +1,63 @@
-$(document).ready(function() {
-	teams = [
-		{
-			name: "The Journey",
-			members: ["akb","duncan","krall","shaon"],
-			description: "AWESOMENESS",
-			stats: {
-				useful: 1,
-				funny: 5,
-				cool: 3,
-				bling: 2
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "Some blokes",
-			members: ["jeff", "ashleykb", "duncan", "shaon"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "lskjd lakj ",
-			members: ["jeff", "jim"],
-			description: "Wat wat in tha butt.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
-		},
-		{
-			name: "The wattt",
-			members: ["jeff", "jim"],
-			description: "Wat.",
-			stats: {
-				useful: 3,
-				funny: 4,
-				cool: 5,
-				bling: 1
-			}
+(function() {
+
+	$(document).ready(function() {
+		$.ajax('/my_votes', {
+			success: showVotes
+		})
+	});
+
+	function showVotes(teams) {
+		for (var i=0; i<teams.length; i++) {
+			team = teams[i];
+			sMembers = team.members.join(', ');
+			var team_votes = $('<div class="team_votes" style="display:none"/>');
+			var labels_and_scores = [["Useful", team.stats.USEFUL], ["Funny", team.stats.FUNNY ], ["Cool", team.stats.COOL], ["Bling", team.stats.BLING]];
+
+			$.each(labels_and_scores, function(index, label_and_score) {
+				var rating_container = $('<div class="rating-container" />');
+				var label_container = $('<div class="type-desc" />').append($("<span />").text(label_and_score[0]));
+				var rating = $('<div class="rating stars-'+label_and_score[1]+'" />');
+				rating_container.append(label_container);
+				rating_container.append(rating);
+				rating_container.attr('votetype',label_and_score[0].toUpperCase())
+				team_votes.append(rating_container);
+			    });
+
+			var team_desc = $('<div class="team-description" />')
+				.append($('<div class="col1 teamname"/>').text(team.name))
+				.append($('<div class="col2 members"/>').text(team.members.join(', ')))
+
+			$('#hackathon-teams').append(
+				$('<div class="team" />')
+				.attr('teamnum', team.project_id)
+				.append(team_desc)
+				.append(team_votes)
+			)
 		}
-	]
-	
-	for (var i=0; i<teams.length; i++) {
-		team = teams[i];
-		sMembers = team.members.join(', ');
-		var team_votes = $('<div class="team_votes" style="display:none"/>');
-		var labels_and_scores = [["Useful", team.stats.useful], ["Funny", team.stats.funny ], ["Cool", team.stats.cool], ["Bling", team.stats.bling]];
 
-		$.each(labels_and_scores, function(event, node) {
-			var rating_container = $('<div class="rating-container" />');
-			var label_container = $('<div class="type-desc />').append($("<span />").text(node[0]));
-			var rating = $('<div class="rating stars-'+node[1]+'" />');
-			rating_container.append(label_container);
-			rating_container.append(rating);
-			team_votes.append(rating_container);
-		    });
-
-		var team_desc = $('<div class="team-description" />')
-			.append($('<div class="col1 teamname"/>').text(team.name))
-			.append($('<div class="col2 members"/>').text(team.members.join(', ')))
-
-		$('#hackathon-teams').append(
-			$('<div class="team" />')
-			.append(team_desc)
-			.append(team_votes)
-		)
-	}
-
-	ratings = $('.rating')
-	for (var i=1; i<=5; i++) {
-		(function(i) {
-			ratings.append(function(ixRating) {
-				return $('<div class="starArea"></span>')
-					.mouseenter(function(event) {
-						$(ratings[ixRating]).addClass('mouseover-stars'+i)
-					})
-					.mouseleave(function(event) {
-						$(ratings[ixRating]).removeClass('mouseover-stars'+i)
-					})
-					.mouseup(function(event) {
+		ratings = $('.rating')
+		for (var i=1; i<=5; i++) {
+			(function(i) {
+				ratings.append(function(ixRating) {
+					var starArea = $('<div class="starArea"></span>')
+					starArea.click(function (event) {
 						$(ratings[ixRating]).removeClass('stars-1 stars-2 stars-3 stars-4 stars-5').addClass('stars-'+i)
-					})
-			})
-		}(i))
+						project_id = $(event.target).parents('.team').attr('teamnum')
+						vote_type = $(event.target).parents('.rating-container').attr('votetype')
+						$.post('/vote/', {
+							project_id: project_id,
+							vote_type: vote_type,
+							score: i
+						})
+					});
+					return starArea;
+				});
+			}(i))
+		}
+
+		$(".team-description").click(function(){
+			$(this).next().slideToggle();
+		    });
 	}
 
-	$(".team-description").click(function(){
-		$(this).next().slideToggle();
-	    });
-})
-
-
+}());
